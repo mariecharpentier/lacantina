6af4860db842d8df8d7399d05560c2b511f39368
mariecharpentier <44870518+mariecharpentier@users.noreply.github.com>
2020-04-04 12:19:42 +0200

    indentation

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,39 +5,39 @@ const API_ENDPOINT = 'http://localhost:9000/api';
 
 export default {
 
-    getAllRecipes: function() {
-        return axios
+	getAllRecipes: function () {
+		return axios
 			.get(`${API_ENDPOINT}/recipes`)
 			.then((res) => res.data)
 			.catch((error) => console.log(`Ajax error : ${error}`));
-    },
-	
-	getOneRecipe: function(id) {
+	},
+
+	getOneRecipe: function (id) {
 		return axios
 			.get(`${API_ENDPOINT}/recipe/${id}`)
 			.then((res) => res.data)
 			.catch((error) => console.log(`Ajax error : ${error}`));
 	},
 
-    deleteRecipe: function(id) {
+	deleteRecipe: function (id) {
 		return axios
 			.delete(`${API_ENDPOINT}/recipe/${id}`)
 			.then((res) => res.data)
 			.catch((error) => console.log(`Ajax error : ${error}`))
 	},
 
-	createRecipe: function(recipe) {
+	createRecipe: function (recipe) {
 		return axios
 			.post(`${API_ENDPOINT}/recipes/`, recipe)
 			.then((res) => res.data)
 			.catch((error) => console.log(`Ajax error : ${error}`));
 	},
 
-	editRecipe: function(recipe) {
+	editRecipe: function (recipe) {
 		return axios
-		.put(`${API_ENDPOINT}/recipe/${recipe.id}`, recipe)
-		.then((res) => res.data)
-		.catch((error) => console.log(`Ajax error : ${error}`));
+			.put(`${API_ENDPOINT}/recipe/${recipe.id}`, recipe)
+			.then((res) => res.data)
+			.catch((error) => console.log(`Ajax error : ${error}`));
 	}
 
 }
